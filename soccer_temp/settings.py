39af67e155df5d5f3143122aef5c2d2f43095725
tatsukamijo <tatsukamijo@icloud.com>
2022-09-10 11:22:42 +0900

    deploy settings

--- soccer_temp/settings.py
+++ soccer_temp/settings.py
@@ -24,7 +24,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 from .local_settings import SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+try:
+  from .local_settings import *
+except ImportError:
+  pass
+
+if not DEBUG:
+  SECRET_KEY = os.environ['SECRET_KEY']
 
 ALLOWED_HOSTS = []
 
